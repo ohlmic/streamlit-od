616b2c3757dc2c975f5dcdde53e0fe58ed53ef0e
ohlmic <michaelohlsson81@gmail.com>
2022-05-04 15:41:10 +0200

    added code for inference gluoncv 2

--- pages/machine_learning.py
+++ pages/machine_learning.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import gluoncv as gcv
+from gluoncv.utils import viz
+from matplotlib import pyplot as plt
 
 def app():
      st.subheader('Object detection')
+     classes = ['tension_clamp', 'kpo', 'cable']
+     net = gcv.model_zoo.get_model('ssd_512_mobilenet1.0_custom', classes=classes, pretrained_base=False)
+     net.load_parameters('saved_models/ssd_512_mobilenet1.2_railway.params')
+     x, image = gcv.data.transforms.presets.ssd.load_test('od_rail_dataset-holdout-test_01/11003.jpg', 512)
+     cid, score, bbox = net(x)
+     ax = viz.plot_bbox(image, bbox[0], score[0], cid[0], class_names=classes)
+     st.pyplot()
+     #plt.show()
 
-     chart_data = pd.DataFrame(
-          np.random.randn(20, 3),
-          columns=['a', 'b', 'c'])
-
-     st.line_chart(chart_data)
